44e3566479d02b0207005395810de9b4abc46d28
hsiaohungwang <wang@ispan.com.tw>
2024-04-17 15:05:39 +0800

    service worker - install event

---
+++ wwwroot/sw.js
@@ -0,0 +1,16 @@
+﻿const CACHE_NAME = "cache-v1";
+const urlsToCatch = ['/', '/abc.css', '/index.html', '/sw.js'];
+self.addEventListener('install', function (event) {
+    console.log('install');
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCatch);
+        })
+    );
+})
+self.addEventListener('activate', function (event) {
+    console.log('activate');
+})
+self.addEventListener('fetch', function (event) {
+    console.log('fetch');
+})
